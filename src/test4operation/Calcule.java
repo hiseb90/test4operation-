2cd2d5d0d108f2aa93b079b3b28ae466df09dc6d
elboraki <burak7younes@gmail.com>
2016-09-24 22:28:24 +0000

    create a menu

--- src/test4operation/Calcule.java
+++ src/test4operation/Calcule.java
@@ -1,7 +1,31 @@
 package test4operation;
 
+import java.util.Scanner;
+
 public class Calcule {
 	public static void main(String args[]){
 		System.out.println("hello world");
+		//declare input system for user
+		Scanner keyb=new Scanner(System.in);
+		//declare value of menu or number of choice
+		int valOfMenu=0;
+		//while valOfMenu is different to q display this menu
+		while(valOfMenu!=5){
+			System.out.println("-----Menu of Operation Arithmetic---");
+			System.out.println("1--Add ");
+			System.out.println("2--Subtract");
+			System.out.println("3--Divide");
+			System.out.println("4--Multiplication");
+			System.out.println("5-Exit");
+			System.out.println("Please choose one of the selection:");
+			valOfMenu=keyb.nextInt();
+			
+			
+			
+		}
+		
+		
+		
 	}
+	
 }
